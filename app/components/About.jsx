ed46185a95bdd76809e401bb022c0848c50366f3
Andrew <email@andrewlord.co.uk>
2017-04-22 15:11:04 +0100

    Added custom styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p className="lead">Welcome to this <a href="https://facebook.github.io/react/">React</a> Weather App, using the <a href="https://openweathermap.org">OpenWeatherMap.org</a> <abbr title="Application Programming Interface">API</abbr>.</p>
       <p>You can search for a locations, and this app will return the temperature at that location to you.</p>
     </div>
